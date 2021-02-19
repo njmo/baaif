c3a37a2787990ef5abeb9486550d8abf3fb04741
njmo <mklonica94@gmail.com>
2021-02-19 20:54:13 +0100

    Toggle led using TIM2 interrupt

--- stm8s105c6t6/main.asm
+++ stm8s105c6t6/main.asm
@@ -17,6 +17,13 @@ UART2_CR3  EQU $5246
 UART2_BBR1	EQU $5242
 UART2_BBR2	EQU $5243
 
+TIM2_CR1 EQU $5300
+TIM2_SR1 EQU $5302
+TIM2_IER EQU $5301
+TIM2_ARRH EQU $530D
+TIM2_ARRL EQU $530E
+TIM2_PSCR EQU $530C
+TIM2_EGR EQU $5304
 
 ; UART_DIV=0x0693 
 ;BBR1=0x68h 
@@ -42,7 +49,7 @@ clear_ram
     cpw X,#$stack_segment_end
     jrule clear_ram
 ; it is now safe to use the stack
-
+		rim
 ; init our program
     bset  PD_DDR,#0        ; PD0 is set to output mode                    
 		mov UART2_CR1,#0 
@@ -52,26 +59,23 @@ clear_ram
 		bset UART2_CR2, #3
 		mov UART2_BBR2,#$03
 		mov	UART2_BBR1,#$68
+		
+		bset TIM2_IER,#0
+		mov TIM2_PSCR,#8
+		mov TIM2_ARRH,#$F4
+		mov TIM2_ARRL,#$24
+
+		bset TIM2_CR1, #0
 	
 ;ldf A,$5240
 ; main program loop
-loop_forever                                    
-    call blink_once
-		call print_once
+loop_forever
     jra loop_forever
 
 ; blink is now a subroutine
 ; this delays for about a second then toggles the
 ; LED On or Off
 blink_once
-    ldw X,#$FFFF    
-loop_delay
-    ldw Y,#$0050    
-loop_inner_delay
-    decw Y
-    jrne loop_inner_delay
-    decw X
-    jrne loop_delay
     bcpl  PD_ODR,#0   
     ret
 		
@@ -92,6 +96,13 @@ loop
 
 napis: dc.b "Marcin to gej", $d, $a, 0
 LEN_NAPIS	equ	{*-napis}
+
+		interrupt TimerInterrupt
+TimerInterrupt
+    call blink_once
+		mov TIM2_SR1, #0
+;		call print_once
+		iret
 
 ; the default interupt handler
     interrupt NonHandledInterrupt
@@ -115,7 +126,7 @@ NonHandledInterrupt
     dc.l {$82000000+NonHandledInterrupt}    ; irq10 SPI end of transfer
     dc.l {$82000000+NonHandledInterrupt}    ; irq11 TIM1 Update/overflow/underflow/trigger/break
     dc.l {$82000000+NonHandledInterrupt}    ; irq12 TIM1 Capture/Compare
-    dc.l {$82000000+NonHandledInterrupt}    ; irq13 TIM2 update/overflow
+    dc.l {$82000000+TimerInterrupt}    			; irq13 TIM2 update/overflow
     dc.l {$82000000+NonHandledInterrupt}    ; irq14 TIM2 capture / compare
     dc.l {$82000000+NonHandledInterrupt}    ; irq15 TIM3 Update/ overflow
     dc.l {$82000000+NonHandledInterrupt}    ; irq16 TIM3 Capture / Compare
